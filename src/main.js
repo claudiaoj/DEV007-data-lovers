a29916069f381822d30e4dcf77ccb0fe41dc0868
claudiaoj <claudiaortizjabre@gmail.com>
2023-05-07 20:28:56 -0400

    Es el primer avance

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
+import data from './data/athletes/athletes.js';
 console.log(example, data);
+console.log(data.athletes);
+
+
+const nombreAtletas = data.athletes;
+console.log(nombreAtletas);
+for (let i = 0; i < nombreAtletas.length; i++){
+    nombreAtletas[i].name
+    console.log(nombreAtletas[i].name);
+}
+
+
+
