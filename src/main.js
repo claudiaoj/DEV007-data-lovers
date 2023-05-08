99745d0aad8e6de53d9225c063a9928166989c51
claudiaoj <claudiaortizjabre@gmail.com>
2023-05-08 17:41:31 -0400

    muestra los nombres

--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@ import data from './data/athletes/athletes.js';
 console.log(example, data);
 console.log(data.athletes);
 
+const atletas = data.athletes;
+const contenedor = document.getElementById("contenedor");
 
-const nombreAtletas = data.athletes;
+atletas.forEach(atleta => {
+    contenedor.innerHTML += `
+    <h5>${atleta.name}<h5>
+    `
+    //console.log(atleta)
+})
+
+
+
+/*const nombreAtletas = data.athletes;
 console.log(nombreAtletas);
 for (let i = 0; i < nombreAtletas.length; i++){
     nombreAtletas[i].name
     console.log(nombreAtletas[i].name);
-}
+}*/
 
 
 
