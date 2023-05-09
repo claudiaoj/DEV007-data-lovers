4e902dd6ed378e95df8232155d3d9f16201b5600
claudiaoj <claudiaortizjabre@gmail.com>
2023-05-09 14:46:32 -0400

    agrega las cards

--- src/main.js
+++ src/main.js
@@ -6,14 +6,20 @@ console.log(data.athletes);
 const atletas = data.athletes;
 const contenedor = document.getElementById("contenedor");
 
-atletas.forEach(atleta => {
+
+atletas.slice(0,50).forEach(atleta => {
     contenedor.innerHTML += `
-    <h5>${atleta.name}<h5>
+    <di class="card-atleta" id="imagen">
+        <img class="imagen-medalla" src="images/medallas.png" alt="medalla" >
+        <h5>Nombre: ${atleta.name}</h5>
+        <h5>Género: ${atleta.gender}</h5>
+        <h5>País: ${atleta.team}</h5>
+        <h5>Deporte: ${atleta.sport}</h5>
+        <h5>Medalla: ${atleta.medal}</h5>
+    </div>
     `
     //console.log(atleta)
 })
-
-
 
 /*const nombreAtletas = data.athletes;
 console.log(nombreAtletas);
