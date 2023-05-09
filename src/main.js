ab4b4262684f4bdd712cf4b5359f50ce3b2e74ea
GabrielaMolina <gamolinacartes86@gmail.com>
2023-05-09 18:47:47 -0400

    acorta data

--- src/main.js
+++ src/main.js
@@ -9,13 +9,6 @@ const contenedor = document.getElementById("contenedor");
 
 atletas.slice(0,50).forEach(atleta => {
     contenedor.innerHTML += `
-<<<<<<< HEAD
-    <h5>Nombre: ${atleta.name}</h5>
-    <h5>Género: ${atleta.gender}</h5>
-    <h5>País: ${atleta.team}</h5>
-    <h5>Deporte: ${atleta.sport}</h5>
-    <h5>Medalla: ${atleta.medal}</h5>
-=======
     <di class="card-atleta" id="imagen">
         <img class="imagen-medalla" src="images/medallas.png" alt="medalla" >
         <h5>Nombre: ${atleta.name}</h5>
@@ -24,7 +17,6 @@ atletas.slice(0,50).forEach(atleta => {
         <h5>Deporte: ${atleta.sport}</h5>
         <h5>Medalla: ${atleta.medal}</h5>
     </div>
->>>>>>> 4516fa8a7f43222dd645b92d31322b7b0830d900
     `
     //console.log(atleta)
 })
